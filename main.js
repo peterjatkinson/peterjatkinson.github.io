3329b7a6d28442e471cf9383e36ba2dffa58b9fd
Peter Atkinson <peterjatkinson@gmail.com>
2021-03-18 10:46:30 +0000

    Added grid tile page

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ function openCity(cityName) {
   //So if you click on the 'London' button, the click triggers the openCity function and passes 'London' as the argument. And then the getElementById picks up the ID London because, I think, the argument given and the ID of that div are the same. I think that's everything...
 }
 
+
 //  Tabs with jquery
 $(document).ready(function(){
   $("#LondonB").click(function(){
@@ -38,6 +39,7 @@ $(document).ready(function(){
     $("#TokyoB").fadeTo(1, 0.6);
   });
 });
+
 
 // CAROUSEL/SLIDESHOW FUNCTIONS
 var slideIndex = 1;
@@ -65,5 +67,65 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
+}
+
+
+// ************SORTABLE TABLE SCRIPT
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+
+  //   I added the below line of code so that the table variable is always the actual table clicked on. The parentElement appendages go up the DOM until it hits the <table> element (from <th> on event.target, to <tr>, to <thead> (added by browser even if I don't include it), to <table>. 'event.target' starts at the element that caused the event (the one clicked on), in this case one of the th elements. This means that I only need one bit of code for all tables set up this way. 
+  table = event.target.parentElement.parentElement.parentElement;
+  
+  switching = true;
+  //Set the sorting direction to ascending:
+  dir = "asc"; 
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /*check if the two rows should switch place,
+      based on the direction, asc or desc:*/
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      //Each time a switch is done, increase this count by 1:
+      switchcount ++;      
+    } else {
+      /*If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again.*/
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
 }
 
