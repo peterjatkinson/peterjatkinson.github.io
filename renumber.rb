b611e3d37d461fc0cca0d77438ca6c80c346d40d
Peter Atkinson <peterjatkinson@gmail.com>
2021-05-28 11:01:47 +0100

    Added renumbering program for footnote changes

--- renumber.rb
+++ renumber.rb
@@ -37,17 +37,55 @@ for i in 0..(length)
   increment = increment + 1   
 end
 
-# # RESET ALL INLINE HREF SINGLE DIGIT NOs TO 0
-# chapter.gsub!(/(?<=<a id=\"_ftnref\d\" href=\'#_ftn)\d\'/, "0'")
+#RESET ALL FOOTNOTE HREFs TO 0, AND ALL FOOTNOTE ACTUAL TEXT NUMBERS TO 0
+chapter.gsub!(/(?<=#_ftnref)\d+\'>\[\d+/, "0\'>[0")
 
-# #REPLACE ALL INLINE HREF SINGLE DIGIT NUMBERS WITH INCREMENTAL NUMBERING STARTING AT 1
-# idnos = chapter.scan(/(?<=<a id=\"_ftnref\d\" href=\'#_ftn)\d\'/).to_a
-# length = idnos.size
-# increment = 1
+#REPLACE ALL FOOTNOTE HREF NUMBERS WITH INCREMENTAL NUMBERING STARTING AT 1
+hrefnos = chapter.scan(/(?<=#_ftnref)0/).to_a
+length = hrefnos.size
+increment = 1
 
-# for i in 0..(length)
-#   chapter.sub!(/(?<=<a id=\"_ftnref\d\" href=\'#_ftn)\d/) { Regexp.last_match[0].to_i + increment } 
-#   increment = increment + 1   
-# end
+for i in 0..(length)
+  chapter.sub!(/(?<=#_ftnref)0/) { Regexp.last_match[0].to_i + increment } 
+  increment = increment + 1   
+end
 
-puts chapter
+#REPLACE ALL FOOTNOTE ACTUAL WITH INCREMENTAL NUMBERING STARTING AT 1
+actualnos = chapter.scan("[0]")
+length = actualnos.size
+increment = 1
+
+for i in 0..(length)
+  chapter.sub!(/(?<=\[)0/) { Regexp.last_match[0].to_i + increment } 
+  increment = increment + 1   
+end
+
+#RESET ALL INLINE HREFs TO 0, AND ALL INLINE ACTUAL TEXT NUMBERS TO 0
+chapter.gsub!(/(?<=#_ftn)\d+\'>\[\d+/, "0\'>[0")
+
+#REPLACE ALL INLINE HREF NUMBERS WITH INCREMENTAL NUMBERING STARTING AT 1
+hrefnos = chapter.scan(/(?<=#_ftn)0/).to_a
+length = hrefnos.size
+increment = 1
+
+for i in 0..(length)
+  chapter.sub!(/(?<=#_ftn)0/) { Regexp.last_match[0].to_i + increment } 
+  increment = increment + 1   
+end
+
+#REPLACE ALL INLINE ACTUAL NUMBERS WITH INCREMENTAL NUMBERING STARTING AT 1
+actualnos = chapter.scan("[0]")
+length = actualnos.size
+increment = 1
+
+for i in 0..(length)
+  chapter.sub!(/(?<=\[)0/) { Regexp.last_match[0].to_i + increment } 
+  increment = increment + 1   
+end
+
+# puts chapter
+
+#OUTPUT THE REVISED CONTENTS OF chapter TO THE SAME FILE THAT WAS PUT INTO THE COMMAND LINE ARGUMENT AT THE START
+output = File.open( fh, "w" )
+output << "#{chapter}"
+output.close   
